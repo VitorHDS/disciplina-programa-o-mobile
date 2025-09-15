60c324002f54e0f430ee6bf860f79929b39f3a18
Vitor Hugo <santos.hugo1@aluno.ifsp.edu.br>
2025-09-15 11:45:13 -0300

    Atividade 18

--- Atividade 2/Atividade_17/atividade_17.gd
+++ Atividade 2/Atividade_17/atividade_17.gd
@@ -1,18 +1,1 @@
 extends Control
-
-
-func _on_button_pressed() -> void:
-
-	if $OptionButton.get_selected_id() == 0:
-		$Label.text = str (int($TextEdit.text) + int($TextEdit2.text))
-		
-	elif $OptionButton.get_selected_id() == 1:
-		$Label.text = str (int($TextEdit.text) - int($TextEdit2.text))
-		
-	elif $OptionButton.get_selected_id() == 2:
-		$Label.text = str (int($TextEdit.text) * int($TextEdit2.text))
-		
-	elif $OptionButton.get_selected_id() == 3:
-		$Label.text = str (float($TextEdit.text) / float($TextEdit2.text))
-		
-	pass
