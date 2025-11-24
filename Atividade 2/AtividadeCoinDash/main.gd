641878f2d50693997e5e435ed48d65c8b2b37228
Vitor Hugo <santos.hugo1@aluno.ifsp.edu.br>
2025-11-24 11:49:17 -0300

    Atividade 25 - CoinDash

--- Atividade 2/AtividadeCoinDash/main.gd
+++ Atividade 2/AtividadeCoinDash/main.gd
@@ -11,7 +11,7 @@ var playing = false
 func _ready():
 	screensize = get_viewport().get_visible_rect().size
 	$Player.screensize = screensize
-	$Player.hide()
+	#$Player.hide()
 	
 func new_game():
 	playing = true
@@ -28,11 +28,13 @@ func spawn_coins():
 		var c = coin_scene.instantiate()
 		add_child(c)
 		c.screensize = screensize
-		c.position = Vector2(randi_range(0, screensize.x))
-			randi_range(0, screensize.y)
+		c.position = Vector2(randi_range(0, screensize.x), randi_range(0, screensize.y))
 
 func _process(delta):
-	if playing and
+	if playing and get_tree().get_nodes_in_group("coins").size() == 0:
+		level += 1
+		time_left += 5
+		spawn_coins()
 		
 		
 		
